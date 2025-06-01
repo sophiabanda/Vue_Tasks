0c8c487bdf4a8cdd887ae3cc9f0565f2339ed209
Sophia M Banda <Banda@soapity.lan>
2025-06-01 10:33:07 -0400

    Updated checkbox to emit to app component

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -4,13 +4,22 @@ import type { Task } from "../types";
 const props = defineProps<{ 
     tasks: Task[] 
 }>();
+
+const emits = defineEmits<{
+    toggleDone: [id: string]
+}>();
+
 </script>
 
 <template>
     <div class="task-list">
         <article v-for="task in props.tasks" :key="task.id">
             <label>
-                <input v-model="task.done" type="checkbox">
+                <input 
+                type="checkbox"
+                :checked="task.done"
+                @input="emits('toggleDone', task.id)"
+                >
                 {{ task.title }}
             </label>
         </article>
@@ -22,4 +31,6 @@ const props = defineProps<{
         margin-top: 1rem;
     }
     </style>
-    <!-- v-model binds the input to task.done -->
+    <!-- v-model binds the input to task.done. While you can do this,
+     it's best practive to only modify state where it's defined, 
+     so we instead will use emits-->
