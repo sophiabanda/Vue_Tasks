5b1b03800908c06e5c88339f3ca5d2fe2709e48b
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-01 12:56:08 -0400

    Filtering functionality

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import { ref, computed } from "vue";
 import TaskForm from "./components/TaskForm.vue";
 import TaskList from "./components/TaskList.vue"
-import type { Task } from "./types";
+import FilterButton from "./components/FilterButton.vue";
+import type { Task, TaskFilter } from "./types";
 
 const message = ref("Tasks App");
 const tasks = ref<Task[]>([]);
 // 'typed' in separate file
+const filter = ref<TaskFilter>("all");
 
 const totalDone = computed(() => tasks
 .value
@@ -15,6 +17,18 @@ const totalDone = computed(() => tasks
 // whatever it returns will get stored in this variable (tasks) as a
 //reactive variable. If any dependencies change it will always have the
 // latest value. Only runs if dependency changes!
+
+const filteredTasks = computed(() => {
+  switch(filter.value) {
+    case "all":
+      return tasks.value;
+    case "done":
+      return tasks.value.filter((task) => task.done);
+    case "todo":
+      return tasks.value.filter((task) => !task.done);
+  }
+  return tasks.value;
+})
 
 function addTask(newTask: string) {
   tasks.value.push({
@@ -38,6 +52,10 @@ function removeTask(id: string) {
   }
 }
 
+function setFilter(value: TaskFilter) {
+  filter.value = value; 
+}
+
 </script>
 
 <template>
@@ -50,7 +68,12 @@ function removeTask(id: string) {
      <h3 v-if="!tasks.length">Add a task to get started.</h3>
      <h3 v-else>{{  totalDone }} / {{ tasks.length }} tasks completed.</h3>
     <!-- Conditionally render the h3 with v-if -->
-     <TaskList :tasks @toggle-done="toggleDone" @remove-task="removeTask"/>
+     <div v-if="tasks.length" class="button-container">
+      <FilterButton filter="all" @set-filter="setFilter"/>
+      <FilterButton filter="todo" @set-filter="setFilter"/>
+      <FilterButton filter="done" @set-filter="setFilter"/>
+     </div>
+     <TaskList :tasks="filteredTasks" @toggle-done="toggleDone" @remove-task="removeTask"/>
   </main>
 </template>
 
@@ -62,5 +85,6 @@ function removeTask(id: string) {
   .button-container {
     display: flex;
     justify-content: end;
+    gap: 0.5rem;
   }
 </style>
