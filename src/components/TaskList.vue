84587a5aa4b026969a521bc3359abeaec8a1f50e
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-01 11:33:50 -0400

    Added task list animation via vue

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -13,24 +13,34 @@ const emits = defineEmits<{
 </script>
 
 <template>
-    <div class="task-list">
-        <article class="task" v-for="task in props.tasks" :key="task.id">
-            <label>
-                <input 
-                type="checkbox"
-                :checked="task.done"
-                @input="emits('toggleDone', task.id)"
-                >
-                <span :class="{ done: task.done }">{{ task.title }}</span>
-            </label>
+        <TransitionGroup name="task-list" tag="div" class="task-list">
+            <article class="task" v-for="task in props.tasks" :key="task.id">
+                <label>
+                    <input 
+                    type="checkbox"
+                    :checked="task.done"
+                    @input="emits('toggleDone', task.id)"
+                    >
+                    <span :class="{ done: task.done }">{{ task.title }}</span>
+                </label>
             <button @click="emits('removeTask', task.id)" class="outline">Remove</button>
         </article>
-    </div>
+    </TransitionGroup>
 </template>
 
 <style>
     .task-list {
         margin-top: 1rem;
+    }
+
+    .task-list-enter-active,
+    .task-list-leave-active {
+        transition: all 0.5s ease;
+    }
+    .task-list-enter-from,
+    .task-list-leave-to {
+        opacity: 0;
+        transform: translateX(30px);
     }
 
     .task {
